81d338695ed8cf66f0634afbba98f191e1f6e1c2
Jaja-owiny <jasonowiny20@gmail.com>
2026-03-18 02:11:19 +0300

    Removed some uneccessary pages

--- app.py
+++ app.py
@@ -51,15 +51,12 @@ def sitemap():
 
 @app.route('/')
 def index():
-    return render_template('index1.html')
+    return render_template('index.html')
 
 @app.route('/about')
 def about():
     return render_template('about.html')
 
-@app.route('/team')
-def team():
-    return render_template('team.html')
 
 @app.route('/projects')
 def projects():
@@ -187,5 +184,6 @@ def method_not_allowed(e):
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    #app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(debug=True)
 
